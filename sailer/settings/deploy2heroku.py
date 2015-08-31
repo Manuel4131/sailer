992ca1d80f72e917afd1c81766537ead3235cba7
Manuel4131 <Manual131419@gmail.com>
2015-08-31 22:29:08 +0800

    Add a Herorku deployment configuration file

---
+++ sailer/settings/deploy2heroku.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from .base import *
+import dj_database_url
+
+# Turn off the debug mode.
+DEBUG = False
+
+# Set up the secret key
+SECRET_KEY = get_env_var('DJANGO_SECRET_KEY')
+
+#... Set this up if you can take over the proxy server I thought.
+SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
+
+# root directory of the static files.
+STATIC_ROOT= 'static'
